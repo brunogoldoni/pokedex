b43e7f9c26edefe0af8ec8ea63da4799cf9d6f9f
bruno <bgoldoni@blupay.com.br>
2021-06-25 20:55:50 -0300

    create folders and adjust files

--- App.js
+++ App.js
@@ -7,22 +7,17 @@
  */
 
 import React from 'react';
-import {StatusBar, SafeAreaView, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
+
+import Home from './src/pages/Home';
 
 const App = () => {
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <>
       <StatusBar barStyle="light-content" />
 
-      <View>
-        <Text style={{fontSize: 22, fontWeight: 'bold'}}>Pokemon App</Text>
-      </View>
-
-      <View style={{marginTop: 8}}>
-        <Text style={{fontSize: 18}}>React Native</Text>
-      </View>
-    </SafeAreaView>
+      <Home />
+    </>
   );
 };
 
---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#196CBD',
+
+  dark: '#000000',
+
+  white: '#FFFFFF',
+};
